e052119535b1301a2c7e9e3994fece4e994d0c92
“LuciferStix” <akarsit.singh.154@gmail.com>
2023-08-27 15:59:08 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import eventRouter from "./routes/event_routes.js";
-import router from "./routes/user_routes.js";
+// import eventRouter from "./routes/event_routes.js";
+// import router from "./routes/user_routes.js";
 import cors from "cors";
 
 dotenv.config();
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/user", router);
-app.use("/api/blog", eventRouter);
+// app.use("/api/user", router);
+// app.use("/api/blog", eventRouter);
 
 const PORT = process.env.PORT || 8000;
 
