d68f1240fa5b9bbd5dc2720615876308854b2850
“LuciferStix” <akarsit.singh.154@gmail.com>
2023-08-27 16:05:24 +0530

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 // app.use("/api/user", router);
 // app.use("/api/blog", eventRouter);
